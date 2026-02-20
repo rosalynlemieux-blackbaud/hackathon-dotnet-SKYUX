f8c7b92bf93bc35c30c6812d3ec09af016bb3a27
Rosalyn Lemieux <rosalyn.lemieux@blackbaud.com>
2026-02-20 12:35:09 -0500

    Ignore JSON reference cycles for API responses

--- backend/src/Blackbaud.Hackathon.Platform.Service/Program.cs
+++ backend/src/Blackbaud.Hackathon.Platform.Service/Program.cs
@@ -10,11 +10,15 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.IO.Compression;
 using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
